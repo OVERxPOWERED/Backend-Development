d2b9758a115046f768b237f89096d95beb66f383
OVERxPOWERED <6burhanuddin6@gmail.com>
2025-11-06 08:47:20 +0530

    Start the backend Learning

---
+++ 01_Basics/0_Intoduction.js
@@ -0,0 +1,14 @@
+// => File Structure that is going to be used in the backend.
+
+// -> Files
+// - index.js - DB connection
+// - App.js - Configurations
+// -constants - Constants such as PORT, HOST, DB name etc.
+
+// -> Directories
+// - DB
+// - Models
+// Controllers
+// - Routes
+// - Middlewares
+// - Utils
