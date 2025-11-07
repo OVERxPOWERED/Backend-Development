0f0f0fd8de75dcd7aa6601009cf9449d0dd98077
OVERxPOWERED <6burhanuddin6@gmail.com>
2025-11-07 08:43:39 +0530

    Add info about how backend works

---
+++ 01_Basics/01_How_it_works.js
@@ -0,0 +1,9 @@
+// => There are several packages used for building a backend in javascript.
+// - What I am using is express.
+
+// => What is express?
+// - Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.
+// - It facilitates the rapid development of Node based web applications.
+// - It does the main work of constantly listning to the requests and sending responses, this operation is called Listening.
+
+// => [Note - "/" is known as the home route]
