7f0f85c48a3ba38608022f03ff5466c76b4185de
OVERxPOWERED <6burhanuddin6@gmail.com>
2025-11-07 22:01:58 +0530

    Add basic example of how a project is hosted.

---
+++ 01_Basics/04_How_to_deploy.js
@@ -0,0 +1,25 @@
+// There a few differences between a basic practice code and a production code.
+// - The main difference is that a production code is more secure and optimised.
+// - It is also more scalable and maintainable.
+// - It is also more robust and reliable.
+
+
+// => For now I am going to learn about dotenv package to make my code more secure.
+// - It is used to store sensitive information like API keys, database credentials etc.
+// - It is also used to store environment variables.
+// - It is also used to store configuration variables.
+
+// - Just go to any deployment service provider.
+// - Give them the url of your app either through gitHub, gitLab, dockerHub etc.
+// - setup the environment variables.
+// - Choose the plan that you want.
+// - Click compleete setup button.
+// - They will deploy your app for you.
+
+// - Example 
+// - We can use Digital Ocean to deploy our app.
+// - Just go to Digital Ocean and create a new droplet.
+// - Give them the url of your app either through gitHub, gitLab, dockerHub etc.
+// - setup the environment variables.
+// - Choose the plan that you want.
+// - Click compleete setup button.
